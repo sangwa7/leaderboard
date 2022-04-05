f805865c4781262ad9ef9e6f6c14501169ff1d1f
sangwa7 <didiersangwa@gmail.com>
2022-04-05 12:48:20 +0200

    Fix linters

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import './css/style.css';
 
-import { tasks } from './javascript/storage.js';
+import tasks from './javascript/storage.js';
 
 const ul = document.querySelector('.list');
 
-const task = ()=> {
+const task = () => {
   tasks.forEach((task) => {
     const li = document.createElement('li');
-    li.innerText = "name: " + task.name;
+    li.innerText = `name: ${task.name}`;
     ul.appendChild(li);
   });
-}
+};
 
 task();
--- src/javascript/storage.js
+++ src/javascript/storage.js
@@ -1,9 +1,11 @@
-export const tasks = [
-    {name: 23},
-    {name: 123},
-    {name: 20},
-    {name: 456},
-    {name: 213},
-    {name: 780},
-    {name: 423},
-]
+const tasks = [
+  { name: 23 },
+  { name: 123 },
+  { name: 20 },
+  { name: 456 },
+  { name: 213 },
+  { name: 780 },
+  { name: 423 },
+];
+
+export default tasks;
