8c86f8dd011d1e1993502f82a72bb0440c6beb86
sangwa7 <didiersangwa@gmail.com>
2022-04-05 12:29:28 +0200

    Add ES6 Structure

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import './css/style.css';
+
 import { tasks } from './javascript/storage.js';
 
 const ul = document.querySelector('.list');
 
-function taskList() {
+const task = ()=> {
   tasks.forEach((task) => {
     const li = document.createElement('li');
-    li.innerText = "name:" + task.name;
+    li.innerText = "name: " + task.name;
     ul.appendChild(li);
   });
 }
 
-taskList();
+task();
